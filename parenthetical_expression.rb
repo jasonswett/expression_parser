5476713148711bd97736095712c8e2af45ef8233
Jason Swett <jason@benfranklinlabs.com>
2024-09-18 18:47:54 -0400

    Refactoring.

--- parenthetical_expression.rb
+++ parenthetical_expression.rb
@@ -1,18 +1,29 @@
 class ParentheticalExpression
+  REGEX = /([a-zA-Z\d]+)\(([^)]+)\)/
+
   def initialize(value)
     @value = value
   end
 
   def parse
-    regex = /([a-zA-Z\d]+)\(([^)]+)\)/
-    match_data = @value.match(regex)
-    value_before_parentheses = Expression.new(match_data[1]).parse
-    value_inside_parentheses = Expression.new(match_data[2]).parse
-
     ExpressionTree.new(
       root: "*",
       left_child: value_before_parentheses,
       right_child: value_inside_parentheses
     )
+  end
+
+  private
+
+  def value_before_parentheses
+    Expression.new(match_data[1]).parse
+  end
+
+  def value_inside_parentheses
+    Expression.new(match_data[2]).parse
+  end
+
+  def match_data
+    @value.match(REGEX)
   end
 end
